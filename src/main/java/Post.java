9b0080934518a599285ad55d45a1a51620ab4945
SergeyAver <medbed2011@yandex.ru>
2022-06-25 23:56:07 +0400

    NEw

---
+++ src/main/java/Post.java
@@ -0,0 +1,15 @@
+public class Post {
+    Post.birthday = new FormDate();
+    Post.birthday.day = 13;
+    Post.birthday.month = 6;
+    Post.birthday.year = 1999;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+    public static FormDate birthday;
+
+}
+
